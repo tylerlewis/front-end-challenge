8d49910569754e0e4e458834a9c88ed8c30413a4
Veronica Borges <borges.vero@gmail.com>
2014-07-25 12:45:26 -0700

    first commit

---
+++ config/application.rb
@@ -0,0 +1,40 @@
+require File.expand_path('../boot', __FILE__)
+
+
+require "action_controller/railtie" 
+require "action_mailer/railtie" 
+require "active_resource/railtie" 
+require "rails/test_unit/railtie" 
+require "sprockets/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+module FrontEndChallenge
+  class Application < Rails::Application
+   #config.i18n.enforce_available_locales = false
+    I18n.enforce_available_locales = false
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    config.assets.paths << Rails.root.join("app", "assets")
+    
+    config.assets.version = '1.0'
+
+    
+    config.encoding = "utf-8"
+
+    
+    config.assets.initialize_on_precompile = false
+
+    
+    config.assets.precompile +=
+      ['*.js', 'application_page.css']
+
+ 
+
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrontEndChallenge::Application.config.secret_token = 'aa81b1b0d73747b60a54dae8373f2c525a384672e881f0cef6d5902d7f66f303634a679e87f3c8576b2e3b8b0ed8b9c9370d6eba4b49a17f70df0c05530800f1'
