39f91b8d03e5a8acc42a1be0ca3e93ee5ed43f54
Tyler <tyhlewis@gmail.com>
2014-10-25 15:12:25 -0700

    (refactor) refactors PageCtrl controller to move more logic to pageService

--- app/assets/javascripts/page/controllers/PageCtrl.js
+++ app/assets/javascripts/page/controllers/PageCtrl.js
@@ -19,28 +19,16 @@ function($scope, $log, $window, $interval, $http, $modal, $location, $anchorScro
 	$scope.pageService = pageService;
   
   // Store recipients for current message
-  $scope.recipients = [];
+  $scope.recipients = pageService.recipients;
 
   // Store data of current recipient to be added to recipients array
   $scope.currentRecipient = {};
 
   // Add a new recipient to current list
   $scope.addRecipient = function(recipient) {
-
-    // Do not add a recipient unless a valid sms or email address is provided
-    if(!recipient.sms && !recipient.email) {
-      return;
-    }
-
-    var newRecipient = {
-      recipientName: recipient.recipientName,
-      sms: recipient.sms,
-      email: recipient.email
-    };
-
-    $scope.recipients.push(newRecipient);
-    $scope.currentRecipient = {};
-
+    pageService.addRecipient(recipient, function() {
+      $scope.currentRecipient = {};
+    });        
   };
 
   $scope.message = '';
